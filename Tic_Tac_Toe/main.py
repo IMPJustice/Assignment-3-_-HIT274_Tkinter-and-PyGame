8d6ed4f2fbebb6254c3447146029272c3c86e737
Hoang Phuc Huy Nguyen <96994344+IMPJustice@users.noreply.github.com>
2024-10-14 20:19:16 +1100

    Add files via upload

---
+++ Tic_Tac_Toe/main.py
@@ -0,0 +1,15 @@
+"""
+    Application: Tic Tac Toe 1v1
+    Date: 18th Oct 2024
+    Author: Phuong Trinh Vu, Jay Nguyen
+    Github Repo: https://github.com/IMPJustice/Assignment-3-_-HIT274_Tkinter-and-PyGame.git
+    
+"""
+
+import tkinter as tk
+from function import MainMenu
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainMenu(root)
+    root.mainloop()
